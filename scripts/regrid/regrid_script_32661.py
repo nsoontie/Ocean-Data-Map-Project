4b5322106832d0f786235e602f637f78b4171ce5
Jeffrey Dawson <jeffreyd709@gmail.com>
2018-04-03 17:08:14 -0230

    Converts for full map
    * The script now produces the conversion for the full map. 
    * Issue: uses to much ram at high zoom levels

--- scripts/regrid/regrid_script_32661.py
+++ scripts/regrid/regrid_script_32661.py
@@ -1,4 +1,8 @@
 #!env python
+'''
+This script creates the base layer Mercator projection for the world.  
+Note that at higher zoom levels (8 or higher) this script consumes to much ram for most computers to run. This problem is being worked on. 
+'''
 import sys
 import numpy as np
 import math
@@ -17,24 +21,23 @@ numtiles = 2 ** zoom
 numpixels = numtiles * 256
 
 wgs84 = pyproj.Proj(init='EPSG:4326')
-dest = pyproj.Proj(init='EPSG:32661')
+dest = pyproj.Proj(init='EPSG:3857')
 
-x, y = dest(0, 60.0)
-_, lat = dest(math.sqrt(2.) * y, 0., inverse=True)
-
-minx, miny = dest(-45, lat)
-maxx, maxy = dest(135, lat)
+minx, miny = dest(-180.0, -85.06)
+maxx, maxy = dest(180.0, 85.06)
+print "minx: ", minx, "miny: ", miny
+print "maxx: ", maxx, "maxy: ", maxy
 
 dx = (maxx - minx) / numpixels
 dy = (maxy - miny) / numpixels
-
+print "dx: ", dx, "dy: ", dy
 x = minx + dx * np.indices((numpixels, numpixels), np.float32)[0, :, :]
 y = miny + dy * np.indices((numpixels, numpixels), np.float32)[1, :, :]
 
 lon, lat = dest(x, y, inverse=True)
-
-with Dataset("ETOPO1_Bed_g_gmt4.grd", "r") as ds_in:
-    with Dataset("etopo_EPSG:32661_z%d.nc" % zoom, "w", format='NETCDF4') as ds_out:
+print "lon: ", lon, "lat: ", lat
+with Dataset("ETOPO1_Bed_g_gmt4.grd.nc", "r") as ds_in:
+    with Dataset("etopo_EPSG:3857_z%d.nc" % zoom, "w", format='NETCDF4') as ds_out:
         ds_out.createDimension('x', numpixels)
         ds_out.createDimension('y', numpixels)
 
@@ -47,12 +50,12 @@ with Dataset("ETOPO1_Bed_g_gmt4.grd", "r") as ds_in:
         zvar = ds_out.createVariable('z', 'i4', ('y', 'x'))
 
         input_lon, input_lat = np.meshgrid(ds_in.variables['x'][:],
-                                           ds_in.variables['y'][8300:])
+                                           ds_in.variables['y'][0:11000:])
 
         orig_def = SwathDefinition(lons=input_lon, lats=input_lat)
         target_def = SwathDefinition(lons=lon, lats=lat)
 
-        inputdata = ds_in.variables['z'][8300:, :]
+        inputdata = ds_in.variables['z'][0:11000, :]
 
         resampled = resample_custom(
             orig_def,
@@ -64,3 +67,4 @@ with Dataset("ETOPO1_Bed_g_gmt4.grd", "r") as ds_in:
             fill_value=None, nprocs=4)
 
         zvar[:] = resampled.transpose()[::-1, :]
+
